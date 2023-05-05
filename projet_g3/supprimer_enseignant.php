e49f7e3834a6364ab9710bb33d6b4a1ae542e8ac
abdrahman <haimedaabdrahman@gmail.com>
2023-05-05 21:26:25 +0000

    j'ai fait un ptite modification sur la fichie index.php

--- projet_g3/supprimer_enseignant.php
+++
@@ -1,6 +0,0 @@
-<?php
-  include_once "connexion.php";
-  $id_ens= $_GET['id_ens'];
-  $req = mysqli_query($conn , "DELETE FROM enseignant WHERE id_ens = $id_ens");
-  header("Location:enseignant.php")
-?>
--- projet_g3/supprimer_groupe.php
+++
@@ -1,6 +0,0 @@
-<?php
-  include_once "connexion.php";
-  $id_groupe= $_GET['id_groupe'];
-  $req = mysqli_query($conn , "DELETE FROM groupe WHERE id_groupe = $id_groupe");
-  header("Location:groupe.php")
-?>
